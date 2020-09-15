9abf696f90eeaacfaf90a0d4966fb23dfda473e8
saksham9 <agrawal.saksham9@gmail.com>
2020-09-15 14:20:49 +0530

    Inital commit added navbar

---
+++ src/components/my-navbar/mynavbar.component.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../../assets/icons/saksham.jpg';
+import './mynavbar.style.css';
+
+const MyNavbar=()=>{
+    return(
+        <>
+        <Navbar fixed="top" collapseOnSelect expand="md" variant="dark" className="animate-navbar nav-theme justify-content-between">
+        <Navbar.Brand href="#home">
+        <img src={logo} alt="My Portfolio Logo" className="logo"/>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+        </Navbar>
+
+        </>
+    )
+}
+
+export default MyNavbar;
---
+++ src/themes/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+
+    primary: "#0b0c10",
+  
+    textColor: "#66fcf1"
+  
+  };
