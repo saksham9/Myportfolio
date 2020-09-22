01bd2ae77676758802000f37c1ce68b9a49562c1
saksham9 <agrawal.saksham9@gmail.com>
2020-09-22 18:22:02 +0530

    Skills page added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Fade from "react-reveal/Fade";
 import { Parallax } from "react-parallax";
 import Container from "react-bootstrap/Container";
+import Slide from "react-reveal/Slide";
 // components
 
 import MyNavbar from "./components/my-navbar/mynavbar.component"
 import MyCrousal from "./components/my-carousal/my-carousal.component"
 import TitleMessage from "./components/title-message/title-message.components";
-import About from "./pages/about/about.component"
+import About from "./pages/about/about.component";
+import Skills from "./pages/skills/skills.component";
 
 import './App.css';
 
@@ -35,6 +37,13 @@ const App=()=> {
           </div>
         </Parallax>
       </div>
+      {/* Skill Section */}
+      <Container className="container-box rounded">
+        <Slide bottom duration={500}>
+          <hr />
+          <Skills />
+        </Slide>
+      </Container>
     </div>
   );
 }
