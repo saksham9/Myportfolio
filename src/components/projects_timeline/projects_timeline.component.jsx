48d9e382e2ea49456d7d93b5391bbc2c6fd487bc
saksham9 <agrawal.saksham9@gmail.com>
2020-10-06 02:17:50 +0530

    Final changes Made

--- src/components/projects_timeline/projects_timeline.component.jsx
+++ src/components/projects_timeline/projects_timeline.component.jsx
@@ -3,25 +3,17 @@ import { Timeline, Events, UrlButton, ImageEvent } from "@merc/react-timeline";
 import './projects_timeline.style.css';
 // projects
 import L_GetGitHubInfo from "../../assets/img/projects/evernote.png";
-import L_SmartBrain from "../../assets/img/projects/brain.webp";
-import L_RoboFriends from "../../assets/img/projects/Robofriends.webp";
-import L_ProductHuntClone from "../../assets/img/projects/product_hunt_clone.webp";
-import L_PortfolioUsingDjango from "../../assets/img/projects/portfolioUsingDjango.webp";
+import L_EasyFarm from "../../assets/img/projects/Capture.PNG";
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
 
 // skills
 import L_REACT from "../../assets/img/skills/react.svg";
 import L_NODE_JS from "../../assets/img/skills/nodejs.svg";
-import L_EXPRESS from "../../assets/img/skills/express.svg";
 import L_POSTGRESQL from "../../assets/img/skills/postgresql.svg";
 import Image from "react-bootstrap/Image";
-import L_REDUX from "../../assets/img/skills/redux.svg";
 import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
-import L_BOOTSTRAP4 from "../../assets/img/skills/bootstrap-4.svg";
-import L_DJANGO from "../../assets/img/skills/django.svg";
-import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
 import L_GIT from "../../assets/img/skills/github-api.svg";
 
 const ProjectTimeline = () => {
@@ -83,10 +75,10 @@ const ProjectTimeline = () => {
                         </Accordion>
                     </div>
                     <div className="d-flex justify-content-between flex-nowrap text-center">
-                        <UrlButton href="https://akjha96.github.io/getGithubInfo/" target="_blank">
+                        <UrlButton href="" target="_blank">
                         SEE LIVE
                         </UrlButton>
-                        <UrlButton href="https://github.com/akjha96/getGithubInfo" target="_blank">
+                        <UrlButton href="https://github.com/saksham9/Myportfolio" target="_blank">
                         SOURCE CODE
                         </UrlButton>
                     </div>
@@ -94,7 +86,7 @@ const ProjectTimeline = () => {
                 </ImageEvent>
 
                 {/* Farmer portal */}
-                <ImageEvent date="1/10/2020" className="text-center" text="EasyFarm" src={L_SmartBrain} alt="EasyFarm">
+                <ImageEvent date="1/10/2020" className="text-center" text="EasyFarm" src={L_EasyFarm} alt="EasyFarm">
                     <div className="d-flex justify-content-between flex-column mt-1">
                     <div>
                         <Accordion>
@@ -153,10 +145,10 @@ const ProjectTimeline = () => {
                         </Accordion>
                     </div>
                     <div className="d-flex justify-content-between flex-nowrap text-center">
-                        <UrlButton href="https://smart-face-detect-app.herokuapp.com/" target="_blank">
+                        <UrlButton href="" target="_blank">
                         SEE LIVE
                         </UrlButton>
-                        <UrlButton href="https://github.com/akjha96/Smart_Brain" target="_blank">
+                        <UrlButton href="https://github.com/saksham9/EasyFarm" target="_blank">
                         SOURCE CODE
                         </UrlButton>
                     </div>
